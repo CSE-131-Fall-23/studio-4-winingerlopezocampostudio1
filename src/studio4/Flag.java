bf1cc96a749edb54f2dffe82a284b620ca2feb5e
cswin <cswin@DESKTOP-2O1S96I.dhcp.wustl.edu>
2023-10-04 14:14:31 -0500

    10/4 studio fruity mickey mouse

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,36 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+	int redComponent = 255;
+	int greenComponent = 0;
+	int blueComponent = 0;
+	
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		
+		StdDraw.filledRectangle(.5,.95,0.5,0.05);
+		StdDraw.show();
+		
+		StdDraw.setPenColor(254,135,0);
+		StdDraw.filledRectangle(.5,.85,0.5,0.05);
+		StdDraw.show();
+		StdDraw.setPenColor(255,255,96);
+		StdDraw.filledRectangle(.5,.75,0.5,0.05);
+		StdDraw.show();
+		StdDraw.setPenColor(0,196,0);
+		StdDraw.filledRectangle(.5,.65,0.5,0.05);
+		StdDraw.show();
+		StdDraw.setPenColor(0,0,187);
+		StdDraw.filledRectangle(0.5, 0.55, 0.5, 0.05);
+		StdDraw.show();
+		StdDraw.setPenColor(145,0,187);
+		StdDraw.filledRectangle(0.5, 0.45, 0.5, 0.05);
+		StdDraw.show();
+		
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledEllipse(.5,.7,.2,.1);
+		StdDraw.filledEllipse(0.35, 0.8, .1, .05);
+		StdDraw.filledEllipse(0.65, 0.8, .1, .05);
+		StdDraw.show();
 	}
 }
